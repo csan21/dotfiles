b3a2a50cc377348f32b3a870b8b6bd2e366f91b7
csan <csan.developer@gmail.com>
2018-04-11 23:41:11 -0500

    osx

---
+++ osxdefaults.sh
@@ -0,0 +1,17 @@
+defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool true
+
+defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
+
+defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
+
+defaults write NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled -bool false
+
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+
+defaults write com.apple.finder WarnOnEmptyTrash -bool false
+
+defaults write com.apple.finder AppleShowAllFiles -bool true
+
+sudo nvram SystemAudioVolume=" "
